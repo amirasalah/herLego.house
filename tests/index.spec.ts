924303d27addb9bb7d2896c9a3b5eccc7733cf57
Amira Salah <amera.salah.89@gmail.com>
2024-07-25 13:17:20 +0300

    adding playwright

---
+++ tests/index.spec.ts
@@ -0,0 +1,12 @@
+import test from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.herlego.house/');
+});
+
+test.describe('Her lego house home', () => {
+    test('has header', async ({ page }) => {
+      const header = await page.locator('header');
+      await header.isVisible();
+    });
+});
