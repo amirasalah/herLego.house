097d0cf0e360dc3f8f6703fcd7aced7f16ffa567
Amira Salah <amera.salah.89@gmail.com>
2024-08-10 16:44:04 +0300

    adding vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,9 @@ export default defineConfig({
   site: 'https://herlego.house/',
   integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 }); 
