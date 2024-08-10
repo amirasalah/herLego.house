8e5bced70d0a23821c41b0829521e3e3a86c5143
Amira Salah <amera.salah.89@gmail.com>
2024-08-10 16:14:05 +0300

    add form and viewtransition

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://herlego.house/',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "server"
 });
