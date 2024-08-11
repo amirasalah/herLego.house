58cae47f3aedf3b1b07f3ce48baf5f2435a98ece
Amira Salah <amera.salah.89@gmail.com>
2024-08-11 15:07:06 +0300

    contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
-// import mdx from '@astrojs/mdx';
-// import sitemap from '@astrojs/sitemap';
-// import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://herlego.house/',
   integrations: [tailwind()],
-  // output: "server",
-  // adapter: vercel()
+
 }); 
