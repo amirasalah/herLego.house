c2ea1ec72b078b80afe4ebd274779331ae61a036
Amira Salah <amera.salah.89@gmail.com>
2024-08-12 17:55:48 +0300

    adding sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://herlego.house/',
-  integrations: [tailwind()],
-
-}); 
+  integrations: [tailwind(), sanity({
+    projectId: "awebz0v4",
+    dataset: "production",
+    useCdn: false,
+    studioBasePath: "/studio"
+  }), react()]
+});
