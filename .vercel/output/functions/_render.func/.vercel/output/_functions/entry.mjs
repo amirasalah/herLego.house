a5f0241e12497dacc71edbbae80073a01b606f42
Amira Salah <amera.salah.89@gmail.com>
2024-08-31 18:11:42 +0300

    remove vitest

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_DAxaXTug.mjs';
-import { manifest } from './manifest_CWcnovTZ.mjs';
+import { manifest } from './manifest_Drt_i6Zv.mjs';
 import { onRequest } from './_noop-middleware.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -27,7 +27,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "360f51b2-9b01-4a23-ac8b-75539c3f9d48",
+    "middlewareSecret": "df6ccd04-da31-4566-81bf-fee24690ef58",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
